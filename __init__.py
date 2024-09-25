da53d611fdf39dfe4fb5213c562417091b743418
Lucas Santos Teixeira <chinapirai@gmail.com>
2024-09-24 22:51:35 -0300

    Delete __init__.py

--- __init__.py
+++
@@ -1,5 +0,0 @@
-# IMPORTANT: This File makes Python Recognize this Folder as a Package
-
-# This code lists all the Modules that can be imported from the Core Directory
-
-__all__ = ["validacoes", "utilitarios", "acoes", "dados"]
